47e1d9e5af2b7014298a19696459d7cafd091713
Кирилл Сысоев <k.sysoev@me.com>
2023-03-02 23:29:34 +0800

    Adds controllers for API

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,15 +8,39 @@ import (
 
 const portNumber = ":80"
 
-func main() {
+// App is the application, that contains all the handlers
+type App struct{}
 
+func main() {
+	app := App{}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/token/access", func(w http.ResponseWriter, r *http.Request) {})
-	mux.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {})
-	//Issue access token
-	//Issue refresh token
-	//server public key
+	mux.HandleFunc("/authorize", app.Authorize)
+	mux.HandleFunc("/token", app.Token)
+	mux.HandleFunc("/token/validate", app.ValidateToken)
+	mux.HandleFunc("/public_keys", app.PublicKeys)
+
 	fmt.Println("Stating app at ", portNumber)
 	err := http.ListenAndServe(portNumber, mux)
 	log.Fatal(err)
+}
+
+// Authorize is the handler for /authorize endpoint
+// it will validate authorization code and return jwt token
+func (a *App) Authorize(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Token is the handler for /token endpoint, that will return jwt token
+func (a *App) Token(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// ValidateToken is the handler for /token/validate endpoint, that will validate jwt token
+func (a *App) ValidateToken(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// PublicKeys is the handler for /public_keys endpoint that will return public keys
+func (a *App) PublicKeys(w http.ResponseWriter, r *http.Request) {
+
 }
