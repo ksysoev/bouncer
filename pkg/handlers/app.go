262a00d74a6574712c131dc242abfabad94cb3e5
Кирилл Сысоев <k.sysoev@me.com>
2023-03-13 21:48:29 +0800

    Adds raw implementation for public_keys endpoint

--- pkg/handlers/app.go
+++ pkg/handlers/app.go
@@ -132,5 +132,24 @@ func (a *App) ValidateToken(w http.ResponseWriter, r *http.Request) {
 
 // PublicKeys is the handler for /public_keys endpoint that will return public keys
 func (a *App) PublicKeys(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	certs := a.AppConfig.Certificates
+
+	publicKeys := certs.PublicKeys
+
+	response, err := json.Marshal(publicKeys)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 
 }
