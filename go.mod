d282ee7518b3505c211d20c813c2c3d33858ecdb
Кирилл Сысоев <k.sysoev@me.com>
2023-05-21 13:03:43 +0800

    Adds tests for user model

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/ksysoev/bouncer
 go 1.20
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redismock/v9 v9.0.3 // indirect
 	github.com/golang-jwt/jwt/v5 v5.0.0-rc.1 // indirect
+	github.com/redis/go-redis/v9 v9.0.3 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/models/user_test.go
@@ -0,0 +1,68 @@
+package models
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redismock/v9"
+)
+
+func TestUserModel_GetVersion(t *testing.T) {
+	// Create a mock Redis client
+	client, mock := redismock.NewClientMock()
+
+	// Define the UserModel with the mock Redis client
+	model := &UserModel{
+		Redis:  client,
+		Prefix: "user:",
+		Expiry: time.Hour * 24,
+	}
+
+	// Set the expected Redis command and its return value
+	mock.ExpectTxPipeline()
+	mock.ExpectSetNX("user:123", "0", 0).SetVal(true)
+	mock.ExpectExpire("user:123", time.Hour*24).SetVal(true)
+	mock.ExpectGet("user:123").SetVal("0")
+	mock.ExpectTxPipelineExec()
+
+	// Test the GetVersion method
+	version, err := model.GetVersion(context.Background(), "123")
+	if err != nil {
+		t.Errorf("Error getting version: %v", err)
+		return
+	}
+	if version != "0" {
+		t.Errorf("Version is %s, expected 0", version)
+		return
+	}
+}
+
+func TestUserModel_UpdateVersion(t *testing.T) {
+	// Create a mock Redis client
+	client, mock := redismock.NewClientMock()
+
+	// Define the UserModel with the mock Redis client
+	model := &UserModel{
+		Redis:  client,
+		Prefix: "user:",
+		Expiry: time.Hour * 24,
+	}
+
+	// Set the expected Redis commands and their return values
+	mock.ExpectTxPipeline()
+	mock.ExpectIncr("user:123").SetVal(1)
+	mock.ExpectExpire("user:123", time.Hour*24).SetVal(true)
+	mock.ExpectTxPipelineExec()
+
+	// Test the UpdateVersion method
+	version, err := model.UpdateVersion(context.Background(), "123")
+	if err != nil {
+		t.Errorf("Error updating version: %v", err)
+		return
+	}
+	if version != "1" {
+		t.Errorf("Version is %s, expected 1", version)
+		return
+	}
+}
